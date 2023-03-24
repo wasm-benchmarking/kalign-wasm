16956b18ab91b17406c9ee32d76344d84804f620
Mojgan Asadi <masadi@usc.edu>
2023-03-24 15:27:16 -0700

    Update compile_wasm.sh

--- compile_wasm.sh
+++ compile_wasm.sh
@@ -2,7 +2,9 @@
 
 ./autogen.sh
 emconfigure ./configure
-emmake make EXEEXT=".js" CFLAGS="-O3  $EM_FLAGS"
+#emmake make EXEEXT=".html" CFLAGS="-O3  $EM_FLAGS" LDFLAGS="--shell-file em_shell.html"
+#emmake make EXEEXT=".js" CFLAGS="-O3  $EM_FLAGS" LDFLAGS="--preload-file ../wasm_assets/"
+emmake make EXEEXT=".js" CFLAGS="-O3  $EM_FLAGS" LDFLAGS="--preload-file ../wasm_assets/ -s MODULARIZE=1 -s FORCE_FILESYSTEM=1" 
 mkdir -p build_wasm
 cp src/kalign.* ./build_wasm/
 
